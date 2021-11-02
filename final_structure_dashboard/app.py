70da2f5a6d06f2f0fdb945ffb3414fdb979eec23
ozgurozturk98 <ozgurozturk@live.nl>
2021-11-02 21:57:01 +0100

    Add files via upload

--- final_structure_dashboard/app.py
+++ final_structure_dashboard/app.py
@@ -2,5 +2,5 @@ import dash
 
 # meta_tags are required for the app layout to be mobile responsive
 app = dash.Dash(__name__, suppress_callback_exceptions=True)
-
+app.scripts.config.serve_locally=True
 server = app.server
