55bd8730f9dc8508e88519850f90f6d88de2103e
samkooijman <76946226+samkooijman@users.noreply.github.com>
2021-11-03 17:56:13 +0100

    Add files via upload

--- final_structure_dashboard/index.py
+++ final_structure_dashboard/index.py
@@ -7,14 +7,19 @@ from app import app
 from app import server
 
 # Connect to your app pages
-from apps import appje
+from apps import appje, Regression_USA
 
 
 app.layout = html.Div([
     dcc.Location(id='url', refresh=False),
     html.Div([
         dcc.Link('First Page |', href='/apps/appje'),
-        dcc.Link('Second page', href='/apps/test')
+        dcc.Link('Second page |', href='/apps/test'),
+        dcc.Link('Weather KPI |', href='/apps/weather'),
+        dcc.Link('Twitter API KPI |', href='/apps/twitter'),
+        dcc.Link('Density KPI |', href='/apps/density'),
+        dcc.Link('Covid-19 Measures KPI |', href='/apps/measures'),
+        dcc.Link('Income KPI |', href='/apps/income')
     ], className="row"),
     html.Div(id='page-content', children=[])
 ])
@@ -28,7 +33,17 @@ def display_page(pathname):
     if pathname == '/apps/appje':
         return appje.layout
     elif pathname == '/apps/test':
-        return "Nothing is here"
+        return "Test"
+    elif pathname == '/apps/income':
+        return Regression_USA.layout
+    elif pathname == '/apps/weather':
+        return "weather"
+    elif pathname == '/apps/density':
+        return "density"
+    elif pathname == '/apps/measures':
+        return "measures"
+    elif pathname == '/apps/twitter':
+        return "twitter"
     else:
         return "404 Page Error! Please choose a link"
 
