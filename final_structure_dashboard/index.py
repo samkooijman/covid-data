7d720f923c6b41200c52ddaabc963a061f7afe7d
Zane Storha <35473152+zs16012@users.noreply.github.com>
2021-11-05 01:08:28 +0200

    Add files via upload

--- final_structure_dashboard/index.py
+++ final_structure_dashboard/index.py
@@ -1,28 +1,59 @@
 from dash import dcc
 from dash import html
 from dash.dependencies import Input, Output
-
+import dash_bootstrap_components as dbc
 
 # Connect to main app.py file
 from app import app
 from app import server
 
 # Connect to your app pages
-from apps import appje, density, income
+from apps import appje, political_pref
+SIDEBAR_STYLE = {
+    "position": "fixed",
+    "top": 0,
+    "left": 0,
+    "bottom": 0,
+    "width": "16rem",
+    "padding": "2rem 1rem",
+    "background-color": "#f8f9fa",
+}
+
+
+CONTENT_STYLE = {
+    "margin-left": "18rem",
+    "margin-right": "2rem",
+    "padding": "2rem 1rem",
+}
+
+sidebar = html.Div(
+    [
+        html.H2("COVID DATA", className="display-4"),
+        html.Hr(),
+        html.P(
+            "COVID dashboard", className="lead"
+        ),
+        dbc.Nav(
+            [
+                dbc.NavLink("Vaccination KPI", href='/apps/appje', active="exact"),
+                dbc.NavLink("Political KPI", href='/apps/political_pref', active="exact"),
+                dbc.NavLink("Density KPI", href='/apps/density', active="exact"),
+                dbc.NavLink("Income KPI", href='/apps/income', active="exact"),
+            ],
+            vertical=True,
+            pills=True,
+        ),
+    ],
+    style=SIDEBAR_STYLE,
+)
 
 
 app.layout = html.Div([
     dcc.Location(id='url', refresh=False),
-    html.Div([
-        dcc.Link('First Page |', href='/apps/appje'),
-        dcc.Link('Second page |', href='/apps/test'),
-        dcc.Link('Weather KPI |', href='/apps/weather'),
-        dcc.Link('Twitter API KPI |', href='/apps/twitter'),
-        dcc.Link('Density KPI |', href='/apps/density'),
-        dcc.Link('Covid-19 Measures KPI |', href='/apps/measures'),
-        dcc.Link('Income KPI |', href='/apps/income')
-    ], className="row"),
-    html.Div(id='page-content', children=[])
+    sidebar,
+    html.Div(id='page-content', children=[],
+    style = CONTENT_STYLE
+    )
 ])
 
 
@@ -33,20 +64,14 @@ app.layout = html.Div([
 def display_page(pathname):
     if pathname == '/apps/appje':
         return appje.layout
-    elif pathname == '/apps/test':
-        return "Test"
-    elif pathname == '/apps/income':
-        return income.layout
-    elif pathname == '/apps/weather':
-        return "weather"
+    elif pathname == '/apps/political_pref':
+        return political_pref.layout
     elif pathname == '/apps/density':
-        return density.layout
-    elif pathname == '/apps/measures':
-        return "measures"
-    elif pathname == '/apps/twitter':
-        return "twitter"
-    else:
-        return "404 Page Error! Please choose a link"
+        return ''
+    elif pathname == '/apps/icome':
+        return ''
+
+        
 
 if __name__ == '__main__':
-    app.run_server(host='0.0.0.0', port=8080, debug=False)
+    app.run_server(debug=False)
