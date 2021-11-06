f98b7245334fc765747cc564ba17b54ecfc0e41e
Zane Storha <35473152+zs16012@users.noreply.github.com>
2021-11-07 00:59:13 +0200

    Add files via upload

--- final_structure_dashboard/app.py
+++ final_structure_dashboard/app.py
@@ -1,6 +1,5 @@
 import dash
-
+import dash_bootstrap_components as dbc
 # meta_tags are required for the app layout to be mobile responsive
-app = dash.Dash(__name__, suppress_callback_exceptions=True)
-app.scripts.config.serve_locally=True
+app = dash.Dash(__name__, suppress_callback_exceptions=True, external_stylesheets=[dbc.themes.BOOTSTRAP])
 server = app.server
